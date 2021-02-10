0a5cbae514f022db463a434e32b7709f0b5747e3
yycyuchen <46766273+yycyuchen@users.noreply.github.com>
2021-02-10 00:53:23 -0800

    Update project.py

--- project.py
+++ project.py
@@ -362,7 +362,7 @@ class CarrotCollector(gym.Env):
         returns_smooth = np.convolve(self.returns[1:], box, mode='same')
         plt.clf()
         plt.plot(self.steps[1:], returns_smooth)
-        plt.title('NullCraft')
+        plt.title('Vegetarian')
         plt.ylabel('Return')
         plt.xlabel('Steps')
         plt.savefig('returns.png')
